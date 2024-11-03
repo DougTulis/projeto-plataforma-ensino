fc2f6846fd37d280e2b03299e00974ff461c17aa
Douglas Engratulis <dengratulis@gmail.com>
2024-11-03 17:39:29 -0300

    Criado entidades

---
+++ Entities/Lesson.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_plataforma_ensino.Entities {
+    internal class Lesson {
+    }
+}
